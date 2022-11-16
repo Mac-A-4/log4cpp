966dc028f7e90c6cacca75df1fa35a663c778a5d
Malcolm Alvarez <malcolm.alvarez9898@gmail.com>
2022-11-16 14:34:12 -0800

    Add TAG

--- include/log4cpp/logger/Logger.hpp
+++ include/log4cpp/logger/Logger.hpp
@@ -28,33 +28,33 @@ namespace mdalvz {
 			void flush();
 
 			template <class ... T>
-			void write(Level level, T ... tail) {
+			void write(Level level, const std::string& tag, T ... tail) {
 				if (level < this->level) {
 					return;
 				}
-				std::string data = concat(getTime(), ": [", level, "] ", tail ..., "\n");
+				std::string data = concat(getTime(), ": ", tag, " [", level, "] ", tail ..., "\n");
 				for (auto& writer : writers) {
 					writer->write(data);
 				}
 			}
 
 			template <class ... T>
-			void v(T ... tail) {
+			void v(const std::string& tag, T ... tail) {
 				write(Level::VERBOSE, tail ...);
 			}
 
 			template <class ... T>
-			void d(T ... tail) {
+			void d(const std::string& tag, T ... tail) {
 				write(Level::DEBUG, tail ...);
 			}
 
 			template <class ... T>
-			void i(T ... tail) {
+			void i(const std::string& tag, T ... tail) {
 				write(Level::INFO, tail ...);
 			}
 
 			template <class ... T>
-			void e(T ... tail) {
+			void e(const std::string& tag, T ... tail) {
 				write(Level::ERROR, tail ...);
 			}
 
